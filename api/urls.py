400c28b44f08cd9b2314653667cd6bdfa9adff96
Unknown <maxim.kablov@gmail.com>
2018-11-16 14:21:02 +0300

    Добавлен предварительный ProfilePageView и API для него

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ from api.views import *
 
 urlpatterns = [
     path('auth/', profile.AuthView.as_view()),
+    path('profile/<int:id>/', profile.ProfilePageView.as_view()),
 ]
--- api/views/profile.py
+++ api/views/profile.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from api.models import *
+from api.serializers import *
 
 
 class AuthView(APIView):
@@ -16,3 +17,28 @@ class AuthView(APIView):
                 is_registred = True
                 break
         return Response(is_registered, status = status.HTTP_200_OK)
+
+
+class ProfilePageView(APIView):
+    def get(self, request, id, format = None):
+        profile = Profile.objects.get(external_id = id)
+        serializer = ProfileSerializer(profile, context = {'request': request})
+        return Response(serializer.data, status = status.HTTP_200_OK)
+
+    def put(self, request, id, format = None):
+        profile = Profile.objects.filter(external_id = id)
+
+        if 'first_name' in request.data:
+            new_first_name = request.data['first_name']
+            profile.update(first_name = new_first_name)
+
+        if 'last_name' in request.data:
+            new_last_name = request.data['last_name']
+            profile.update(last_name = new_last_name)
+
+        if 'photo' in request.data:
+            new_photo = request.data['photo']
+            profile.update(photo = new_photo)
+
+        serializer = ProfileSerializer(profile, many = True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
