2ecd78ef03e4ffa2f71ce737e5bc90e011f35cbf
Unknown <maxim.kablov@gmail.com>
2018-11-16 14:20:04 +0300

    Изменен ProfileSerializer

--- api/serializers/profile.py
+++ api/serializers/profile.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from api.models import *
+from .phone import PhoneSerializer
 
 
 class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+    phone = PhoneSerializer()
+
     class Meta:
         model = Profile
-        fields = ('external_id', 'phone', 'first_name', 'last_name', 'photo' 'status')
+        fields = ('external_id', 'phone', 'first_name', 'last_name', 'photo', 'status')
