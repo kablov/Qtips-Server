d3aa5d1e74987eb71eb0268ad5f2d01a49cd81ae
Unknown <maxim.kablov@gmail.com>
2018-11-21 00:56:15 +0300

    Добавлен PhoneNumberVerificationIfAccountDoesntExist и API для него

--- api/urls.py
+++ api/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('signup/', profile.SignUpView.as_view()),
     path('profile/<int:id>/', profile.ProfilePageView.as_view()),
     path('code/', sms_code.PhoneNumberVerificationIfAccountExists.as_view()),
+    path('auth/code/', sms_code.PhoneNumberVerificationIfAccountDoesntExist.as_view()),
 ]
--- api/views/sms_code.py
+++ api/views/sms_code.py
@@ -18,3 +18,19 @@ class PhoneNumberVerificationIfAccountExists(APIView):
             return Response(token.to_dict(), status = status.HTTP_200_OK)
         else:
             raise CodesDoNotMatch("Введенный код не совпадает с отправленным в смс")
+
+
+class PhoneNumberVerificationIfAccountDoesntExist(APIView):
+    def post(self, request, format = None):
+        country_code = request.data['country_code']
+        number = request.data['number']
+        code = request.data['code']
+        phone = Phone.objects.get(Q(country_code = country_code) & Q(number = number))
+        code_in_database = str(SmsCode.objects.get(phone = phone).code)
+        if code == code_in_database:
+            token = Token()
+            token.phone = phone
+            token.save()
+            return Response(token.to_dict(), status = status.HTTP_201_CREATED)
+        else:
+            raise CodesDoNotMatch("Введенный код не совпадает с отправленным в смс")
