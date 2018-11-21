bbcd85fb983d40cb0938df4b22a9897528f09f81
Unknown <maxim.kablov@gmail.com>
2018-11-21 15:48:59 +0300

    Добавлено поле is_verified в модели Phone

--- api/models/phone.py
+++ api/models/phone.py
@@ -9,6 +9,7 @@ class Phone(models.Model):
 
     country_code = models.CharField("Код страны", validators=[MinLengthValidator(2)], max_length = 4)
     number = models.CharField("Номер", validators=[MinLengthValidator(4)], max_length = 14)
+    is_verified = models.BooleanField("Подтвержден?", default = False)
 
     def __str__(self):
         return self.country_code + self.number
