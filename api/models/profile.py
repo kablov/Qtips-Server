11b27afdf769fa5ae3c80bc96045266ffc5246d6
Unknown <maxim.kablov@gmail.com>
2018-11-29 17:09:06 +0300

    Добавлено поле test_balance в модели Profile

--- api/models/profile.py
+++ api/models/profile.py
@@ -31,6 +31,7 @@ class Profile(models.Model):
     qr = models.TextField("QR-код", blank = True, null = True)
     payment_url = models.CharField("URL страницы оплаты", max_length = 45, blank = True)
     balance = models.DecimalField("Баланс", max_digits = 6, decimal_places = 2, default = 0.00)
+    test_balance = models.DecimalField("Тестовый баланс", max_digits = 6, decimal_places = 2, default = 0.00)
 
     def __str__(self):
         return str(self.external_id) + ") " + self.first_name + " " + self.last_name
