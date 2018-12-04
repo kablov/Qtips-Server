bdec90038748c28ea20cbe2a90a2cb3f71fcbed8
Unknown <maxim.kablov@gmail.com>
2018-12-05 01:10:15 +0300

    Добавлена def __str__ в модели Transaction

--- api/models/transaction.py
+++ api/models/transaction.py
@@ -16,3 +16,6 @@ class Transaction(models.Model):
     to_user = models.ForeignKey(Profile, verbose_name = 'Кому', on_delete = models.DO_NOTHING)
     amount = models.DecimalField("Сумма", max_digits = 7, decimal_places = 2, default = 0.00)
     type = models.CharField("Тип транзакции", max_length = 11, choices = type, default = "tip_payment")
+
+    def __str__(self):
+        return str(self.id)
