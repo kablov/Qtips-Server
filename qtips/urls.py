3d107cd99d9ec60e017f7abc6c83164a0701ca6f
Unknown <maxim.kablov@gmail.com>
2018-12-04 16:39:24 +0300

    Добавлен pay/ в эндпоинт для отправки чаевых

--- qtips/urls.py
+++ qtips/urls.py
@@ -7,5 +7,5 @@ from api.views import tip_payment
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('<int:id>/', tip_payment.TipPaymentView.as_view()),
+    path('<int:id>/pay/', tip_payment.TipPaymentView.as_view()),
 ]
