9a59ac534a13561d3b9864204784a87dabadd1f6
Unknown <maxim.kablov@gmail.com>
2018-12-05 00:25:48 +0300

    Изменен TipPaymentView

--- api/views/tip_payment.py
+++ api/views/tip_payment.py
@@ -7,13 +7,13 @@ from api.models import *
 class TipPaymentView(APIView):
     def post(self, request, id, format = None):
         profile = Profile.objects.get(external_id = id)
-        sum = request.data['sum']
+        amount = request.data['amount']
         transaction = Transaction()
         transaction.to_user = profile
         transaction.type = 'tip_payment'
-        transaction.amount = sum
+        transaction.amount = amount
         transaction.save()
 
-        profile.balance += int(sum)
+        profile.balance += int(amount)
         profile.save(update_fields = ['balance'])
         return Response(status = status.HTTP_201_CREATED)
