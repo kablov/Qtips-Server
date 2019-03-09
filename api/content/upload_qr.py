3a4c0fa3864d1d32f05206b01dff04cb99568c46
Unknown <maxim.kablov@gmail.com>
2019-03-09 14:15:06 +0300

    Добавлена функция для загрузки qr-кода на клаудинари

--- api/content/upload_qr.py
+++ api/content/upload_qr.py
@@ -1,8 +1,8 @@
 import cloudinary.uploader
 
 
-def upload_photo(photo, phone):
-    path = "profile/" + str(phone)
+def upload_qr(photo, phone):
+    path = "profile/" + str(phone) + "/qr"
     cloudinary_photo = cloudinary.uploader.upload_resource(photo, public_id = path)
     url = cloudinary_photo.url
     secure_url = str(url).replace("http", "https")
