a6fae80a59bb4c4bed59215b5aa3145ade494988
Unknown <maxim.kablov@gmail.com>
2019-03-30 14:05:38 +0300

    Тест

--- api/serializers/transaction.py
+++ api/serializers/transaction.py
@@ -3,7 +3,7 @@ from api.models import Transaction
 
 
 class TransactionSerializer(serializers.HyperlinkedModelSerializer):
-    time = serializers.DateTimeField(format=("%Y-%m-%d %H:%M:%S.%f%z"))
+    time = serializers.DateTimeField(format=("%Y-%m-%dT%H:%M:%S.%f%z"))
     class Meta:
         model = Transaction
         fields = ('id', 'amount', 'time', 'type')
