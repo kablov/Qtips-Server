feeb847e89e4e4ddc562374f8d10f6c61de0bb10
Unknown <maxim.kablov@gmail.com>
2019-03-30 14:03:10 +0300

    Изменен формат времени в TransactionSerializer

--- api/serializers/transaction.py
+++ api/serializers/transaction.py
@@ -3,6 +3,7 @@ from api.models import Transaction
 
 
 class TransactionSerializer(serializers.HyperlinkedModelSerializer):
+    time = serializers.DateTimeField(format=("%Y-%m-%d %H:%M:%S.%f%z"))
     class Meta:
         model = Transaction
         fields = ('id', 'amount', 'time', 'type')
