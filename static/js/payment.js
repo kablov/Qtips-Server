ab861507f82e20886a0f9d39fb0b7a01a56a6983
Unknown <maxim.kablov@gmail.com>
2019-06-03 20:58:49 +0300

    Добавлены js и js_lib

---
+++ static/js/payment.js
@@ -0,0 +1,10 @@
+﻿$(document).on('click', '#cost > *:nth-child(2) > div', function () {
+    $(this).closest('form').find('input[type="text"]').val($(this).find('p').attr('data-id') + ' Р.')
+})
+
+$(document).on('mouseover', '#cost > *:nth-child(2) > div', function () {
+    $(this).parent().find('> *').each(function () {
+        $(this).attr('class', '_gray')
+    });
+    $(this).attr('class', '_black')
+})
