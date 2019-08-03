ef6148c995cb8dc7a5e1fc688a958c9d6e6c089b
Maxim Kablov <maxim.kablov@gmail.com>
2019-08-03 15:56:52 +0300

    Оптимизирована генерация external_id в модели Profile

--- api/models/profile.py
+++ api/models/profile.py
@@ -11,8 +11,10 @@ class Profile(models.Model):
         verbose_name_plural = 'Профили'
 
     def get_new_external_id():
+        profiles = Profile.objects.all()
+        existing_id_list = [profile.external_id for profile in profiles]
         external_id = random.SystemRandom().randint(100000, 999999)
-        while Profile.objects.filter(external_id=external_id).count() > 0:
+        while external_id in existing_id_list:
             external_id = random.SystemRandom().randint(100000, 999999)
         return external_id
 
